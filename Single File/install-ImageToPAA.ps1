c4a1d1f6947e111977c147adfbc8ab6aba2eee59
TKiraa <jimmydelannoy@gmail.com>
2021-06-26 17:37:15 +0200

    check if that is the correct directory

--- Single File/install-ImageToPAA.ps1
+++ Single File/install-ImageToPAA.ps1
@@ -14,23 +14,33 @@ function AddContextMenu {
     #Need to change it
     $Private:ImageToPAA = "PATH\TO\Steam\steamapps\common\Arma 3 Tools\ImageToPAA\ImageToPAA.exe"
 
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
-    New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
 
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
-    New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+    if ((Get-Item -Path $ImageToPAA -ErrorAction SilentlyContinue) -eq $null) {
 
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
-    New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+      Write-Host "ImageToPAA.exe file was not detected in this location : '$($ImageToPAA)',`n`nOpen the file 'Single File\install-ImageToPAA' and on line 15, change the location of the ImageToPAA.exe file.`n" -ForegroundColor yellow -BackgroundColor black
+      Write-Host "Press any key to exit" -ForegroundColor White
+      $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
-    New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+    }else{
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
+        New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.png\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
+        New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.tga\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
+        New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA –Value 'Convert into PAA with ImageToPAA' -Force
+        New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA -Name 'Icon' -PropertyType String -Value "$Private:ImageToPAA,0"
+        New-Item -Path Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.jpeg\shell\ImageToPAA\command –Value "$Private:ImageToPAA ""%1""" -Force
+    }
 }
 
 #Need Administrator approval
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 AddContextMenu
